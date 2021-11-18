cbc50f3df67a2bfc2581f3b7ad9c3e16ae115a23
kratlis <kwork@fduch.ru>
2021-11-18 06:29:33 +0300

    extract TextReader.java to package utils

--- src/main/java/lab1_closed_key/utils/InitProperties.java
+++ src/main/java/lab1_closed_key/utils/InitProperties.java
@@ -1,5 +1,7 @@
 package lab1_closed_key.utils;
 
+import utils.TextReader;
+
 public class InitProperties {
     public static int OFFSET = 3;
     public static String TEXT_FILE_PATH = "src/test/resources/lab1_closed_key/text.txt";
